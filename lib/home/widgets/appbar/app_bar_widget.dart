150938b73113065fde9a5c80861a7bb3a2902cbb
Guilherme Aguiar <kadeguilherme@gmail.com>
2021-04-27 11:08:12 -0300

    Finalizando o AppBar. Utilizamos muito o Widget Stack

--- lib/home/widgets/appbar/app_bar_widget.dart
+++ lib/home/widgets/appbar/app_bar_widget.dart
@@ -1,5 +1,6 @@
 import 'package:devquiz/core/app_text_styles.dart';
 import 'package:devquiz/core/app_gradients.dart';
+import 'package:devquiz/home/widgets/score_card/score_card_widget.dart';
 import 'package:flutter/material.dart';
 
 class AppBarWidget extends PreferredSize {
@@ -11,23 +12,48 @@ class AppBarWidget extends PreferredSize {
             decoration: BoxDecoration(
               gradient: AppGradients.linear,
             ),
-            child: Row(
+            child: Stack(
               children: [
-                Text(
-                  "Olá, Guilherme",
-                  style: AppTextStyles.title,
-                ),
                 Container(
-                  width: 58,
-                  height: 58,
+                  height: 161,
+                  width: double.maxFinite,
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
                   decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(40),
-                    image: DecorationImage(
-                      image: NetworkImage(
-                          "https://avatars.githubusercontent.com/u/42500464?s=400&u=a049264c93bfb80260b09e275b9e83430e4218c2&v=4"),
-                    ),
+                    gradient: AppGradients.linear,
                   ),
-                )
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text.rich(
+                        TextSpan(
+                          text: "Olá, ",
+                          style: AppTextStyles.title,
+                          children: [
+                            TextSpan(
+                              text: "Guilherme",
+                              style: AppTextStyles.titleBold,
+                            )
+                          ],
+                        ),
+                      ),
+                      Container(
+                        width: 58,
+                        height: 58,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(40),
+                          image: DecorationImage(
+                            image: NetworkImage(
+                                "https://avatars.githubusercontent.com/u/42500464?s=400&u=a049264c93bfb80260b09e275b9e83430e4218c2&v=4"),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Align(
+                  alignment: Alignment(0.0, 1.0),
+                  child: ScoreCardWidget(),
+                ),
               ],
             ),
           ),
